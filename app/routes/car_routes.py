d1335a5accd43379c685cc84ef8752c79823224e
LUCIANO <luciano@iMac-de-LUCIANO.local>
2026-05-24 11:24:39 +0200

    añadir busqueda profesional en deals

--- app/routes/car_routes.py
+++ app/routes/car_routes.py
@@ -1,16 +1,28 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 
 from app.database.deps import get_db
 from app.models.car import Car
 from app.models.car_schema import CarCreate
 from app.services.car_analyzer import analyze_car_deal
 
-router = APIRouter(prefix="/cars", tags=["Cars"])
+router = APIRouter(
+    prefix="/cars",
+    tags=["Cars"]
+)
 
+
+# -------------------------
+# CREAR COCHE
+# -------------------------
 
 @router.post("/")
-def create_car(car: CarCreate, db: Session = Depends(get_db)):
+def create_car(
+    car: CarCreate,
+    db: Session = Depends(get_db)
+):
+
     db_car = Car(
         brand=car.brand,
         model=car.model,
@@ -26,22 +38,61 @@ def create_car(car: CarCreate, db: Session = Depends(get_db)):
     return db_car
 
 
+# -------------------------
+# LISTAR COCHES
+# -------------------------
+
 @router.get("/")
-def list_cars(db: Session = Depends(get_db)):
+def list_cars(
+    db: Session = Depends(get_db)
+):
+
     return db.query(Car).all()
 
 
+# -------------------------
+# RANKING CHOLLOS
+# -------------------------
+
 @router.get("/deals")
 def get_deals(
+
+    search: str | None = Query(default=None),
+
     brand: str | None = Query(default=None),
+
     min_price: float | None = Query(default=None),
+
     max_price: float | None = Query(default=None),
+
     min_year: int | None = Query(default=None),
+
     max_km: int | None = Query(default=None),
+
     only_good: bool = Query(default=False),
+
     db: Session = Depends(get_db)
 ):
+
     query = db.query(Car)
+
+    # -------------------------
+    # SEARCH
+    # -------------------------
+
+    if search:
+
+        query = query.filter(
+
+            or_(
+                Car.brand.ilike(f"%{search}%"),
+                Car.model.ilike(f"%{search}%")
+            )
+        )
+
+    # -------------------------
+    # FILTROS
+    # -------------------------
 
     if brand:
         query = query.filter(Car.brand.ilike(f"%{brand}%"))
@@ -63,6 +114,7 @@ def get_deals(
     analyzed_cars = []
 
     for car in cars:
+
         analyzed = analyze_car_deal(car)
 
         if only_good and not analyzed["good_deal"]:
@@ -70,11 +122,15 @@ def get_deals(
 
         analyzed_cars.append(analyzed)
 
-    analyzed_cars.sort(key=lambda x: x["score"], reverse=True)
+    analyzed_cars.sort(
+        key=lambda x: x["score"],
+        reverse=True
+    )
 
     return {
         "total": len(analyzed_cars),
         "filters": {
+            "search": search,
             "brand": brand,
             "min_price": min_price,
             "max_price": max_price,
